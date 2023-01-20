ec0fcf76cd085b7e77f20ca323c66cb3f366916e
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-20 23:22:47 +0300

    Initial Commit

---
+++ FoursquareCloneParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareCloneParse
+//
+//  Created by Mustafa Adnan Tatlıcı on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
