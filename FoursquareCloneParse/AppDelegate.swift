2055dce425a2516f67fef3d935a86a4d03739858
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-21 01:24:57 +0300

    first commit

--- FoursquareCloneParse/AppDelegate.swift
+++ FoursquareCloneParse/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let configuration = ParseClientConfiguration { ParseMutableClientConfiguration in
+            
+            ParseMutableClientConfiguration.applicationId = "r7q1G45konrjY0EWtcGLuUdRnmfA3JBqqLI6alOd"
+            ParseMutableClientConfiguration.clientKey = "2oCCPiaMfOJAWlKlhhoWL3imXy7yjMIQ9FaPzPBd"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com"
+            
+        }
+
+        Parse.initialize(with: configuration)
+
         return true
     }
 
--- FoursquareCloneParse/ViewController.swift
+++ FoursquareCloneParse/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import Parse
+
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passswordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        if usernameText.text != "" && passswordText.text != "" {
+            PFUser.logInWithUsername(inBackground: usernameText.text!, password: passswordText.text!) { user, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    //segue
+                    print("giriş yapıldı")
+                }
+                     
+            }
+        } else {
+            makeAlert(titleInput: "error", messageInput: "Username/Password??")
+        }
+        
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        print("Tıklandı")
+        if usernameText.text != "" && passswordText.text != "" {
+            let user = PFUser()
+            user.username = usernameText.text!
+            user.password = passswordText.text!
+            
+            user.signUpInBackground { success, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    //segue
+                    print("ok")
+                }
+            }
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "Username/Password??")
+        }
+        
+        
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
